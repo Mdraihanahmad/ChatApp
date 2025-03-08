a9a355dfd6fec49338fa7b581d33e4567b367735
Mdraihanahmad <mdraihanahmad47@gmail.com>
2025-03-08 16:36:45 +0530

    add frontend url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['https://chatapp-frontend-g8sy.onrender.com'],
         methods:['GET', 'POST'],
     },
 });
